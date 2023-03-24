4cf9c1f3414d2bcd539a315fbce6dbdfb525c20f
liflaks <85747237+liflaks@users.noreply.github.com>
2023-03-25 02:33:31 +0600

    Initial commit
    adf

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const authRouter = require('./authRouter') 
+const authRouter = require('./authRouter')
 const PORT = process.env.PORT || 5000
 
 const app = express()
--- middlewaree/roleMiddleware.js
+++ middlewaree/roleMiddleware.js
@@ -1,25 +1,29 @@
+const jwt = require('jsonwebtoken')
+const {secret} = require('../config')
+
+
 module.exports = function (roles) {
-    return function (req, res, next){
-        if(req.method === "OPTIONS") {
+    return function (req, res, next) {
+        if (req.method === "OPTIONS") {
             next()
         }
-    
-        try{
-            const token = req.headers.authtorization.split(' ')[1]
-            if(!token) {
+
+        try {
+            const token = req.headers.authorization.split(' ')[1]
+            if (!token) {
                 return res.status(403).json({message: "Пользователь не авторизован"})
             }
             const {roles: userRoles} = jwt.verify(token, secret)
             let hasRole = false
-            userRoles.array.forEach(role => {
-                if(roles.includes(role)) {
+            userRoles.forEach(role => {
+                if (roles.includes(role)) {
                     hasRole = true
                 }
-            });
-            if(!hasRole) {
+            })
+            if (!hasRole) {
                 return res.status(403).json({message: "У вас нет доступа"})
             }
-            next()         
+            next();
         } catch (e) {
             console.log(e)
             return res.status(403).json({message: "Пользователь не авторизован"})
