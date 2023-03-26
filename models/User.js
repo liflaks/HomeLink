5a9383e98bd13d16d99cc93ccb68ebaa761f617d
liflaks <85747237+liflaks@users.noreply.github.com>
2023-03-26 13:40:16 +0600

    Update User.js
    update

--- models/User.js
+++ models/User.js
@@ -1,14 +1,15 @@
 const {Schema, model} = require('mongoose')
 
 const User = new Schema({
-    username: {type: String, unique: true, required: true}, 
+    username: {type: String, required: true}, 
     password: {type: String, required: true},
     role: {type: String, required: true},
     apps: [{type: String, required: false, default:null}],
     iin: {type: Number, unique: true, required: true},
     zhk: {type: String, required: true},
     appartamentNumber: {type: String, required: true},
-    phoneNumber: {type: String, unique: true, required: true}
+    phoneNumber: {type: String, unique: true, required: true},
+    budget: {type: Number, default: null}
 })
 
 module.exports = model('User', User) 
